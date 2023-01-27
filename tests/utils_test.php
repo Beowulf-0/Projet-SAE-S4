fc7a1ea4173a8e2689a90fdffa905b790ec7949f
A2drien <adriendubois1411@gmail.com>
2023-01-27 15:59:55 +0100

    Ajout de PHPUnit

--- tests/utils_test.php
+++ tests/utils_test.php
@@ -1,3 +1,17 @@
 <?php
 
+
+class UtilsTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp(){
+        require_once __DIR__ . '/../src/functions/utils.php';
+    }
+
+    public function testGetRandomString()
+    {
+        $this->assertEquals(10, strlen(Utils::getRandomString(10)));
+        $this->assertEquals(20, strlen(Utils::getRandomString(20)));
+        $this->assertEquals(30, strlen(Utils::getRandomString(30)));
+    }
+}
 // Développer les tests unitaires pour les fonctions
