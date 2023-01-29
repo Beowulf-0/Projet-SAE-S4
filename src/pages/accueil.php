879ed561ad783f3c15ce4e8b9c0028a630ee7973
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2023-01-29 21:10:26 +0100

    Modifs

--- src/pages/accueil.php
+++ src/pages/accueil.php
@@ -4,33 +4,46 @@
 <head>
     <meta charset="utf-8">
     <title>Formulaire</title>
+    <link rel="stylesheet" type="text/css" href="../style/styleForm.css">
 </head>
 
 <body>
-    <form action="traitement.php" method="post">
+    <form action="traitement.php" method="post" id="form_ident">
+        <label class="titre">Formulaire de renseignement</label>
         <div id="nom-prenom">
-            <label for="nom">Nom</label>
-            <input type="text" name="nom" id="nom" placeholder="Nom" required>
-
-            <label for="prenom">Prénom</label>
-            <input type="text" name="prenom" id="prenom" placeholder="Prénom" required>
+            <div>
+                <label for="nom" class="titre">Nom</label>
+                <input type="text" name="nom" id="nom" placeholder="Nom" required>
+            </div>
+            <div>
+                <label for="prenom" class="titre">Prénom</label>
+                <input type="text" name="prenom" id="prenom" placeholder="Prénom" required>
+            </div>
         </div>
 
+        <div id="adresse-code-postal">
+            <div>
+                <label for="adresse-postale" class="titre">Adresse Postale (facultatif)</label>
+                <input type="text" name="adresse-postale" id="adresse-postale" placeholder="Adresse Postale">
+            </div>
 
-        <label for="date">Date de naissance</label>
-        <input type="date" name="date" id="date" required>
+            <div>
+                <label for="code-postal" class="titre">Code postal (facultatif)</label>
+                <input type="text" name="code-postal" id="code-postal" placeholder="Code postal">
+            </div>
+        </div>
 
-        <label for="tel">Numéro de téléphone</label>
-        <input type="tel" name="tel" id="tel" placeholder="Numéro de téléphone" required>
+        <div id="date-tel-ville">
 
-        <label for="adresse-postale">Adresse Postale</label>
-        <input type="text" name="adresse-postale" id="adresse-postale" placeholder="Adresse Postale">
+            <label for="date" class="titre">Date de naissance</label>
+            <input type="date" name="date" id="date" required>
 
-        <label for="code-postal">Code postal</label>
-        <input type="text" name="code-postal" id="code-postal" placeholder="Code postal">
+            <label for="tel" class="titre">Numéro de téléphone</label>
+            <input type="tel" name="tel" id="tel" placeholder="Numéro de téléphone" required>
 
-        <label for="ville">Ville</label>
-        <input type="text" name="ville" id="ville" placeholder="Ville">
+            <label for="ville" class="titre">Ville</label>
+            <input type="text" name="ville" id="ville" placeholder="Ville">
+        </div>
 
         <!--Mettre une case à cocher pour respecter le RGPD-->
         <label for="rgpd">J'accepte le traitement de mes données personnelles</label>
@@ -41,11 +54,12 @@
         <!--A developper-->
         
 
+        <div id="boutons">
+            <input type="submit" value="Envoyer" id="bouton-validation">
 
-        <input type="submit" value="Envoyer" id="boutton-validation">
-
-        <!--Bouton annulation-->
-        <input type="reset" value="Annuler" id="boutton-annulation">
+            <!--Bouton annulation-->
+            <input type="reset" value="Annuler" id="bouton-annulation">
+        </div>
 
     </form>
 </body>
