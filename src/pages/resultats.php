57762093a6362251abeb8544edde362649801906
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2023-02-12 17:00:12 +0100

    Réalisation de la page résultat.

---
+++ src/pages/resultats.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Résultats sondage</title>
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+    <link rel="stylesheet" type="text/css" href="../style/styleResultat.css">
+</head>
+
+
+<body>
+    <div id="corps">
+        <label for="titre"> <b> Résultats du sondage </b> </label>
+        <div id="graphiques">
+            <img src="../images/graphique.png" class="image" alt="graphique gauche" >
+            <img src="../images/graphique.png" class="image" alt="graphique droit">
+        </div>
+        <div id="bouton">
+            <a id="button" href="../pages/accueil.php"> Retour </a>
+        </div>
+    </div>
+</body>
+
+</html>
