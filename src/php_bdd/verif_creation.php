216cea4c390da17f10ae498a6daf35a449d82b14
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2023-04-04 00:22:19 +0200

    update verif_login/création.php

--- src/php_bdd/verif_creation.php
+++ src/php_bdd/verif_creation.php
@@ -1,5 +1,4 @@
 <?php
-    session_start();
     //require('config.php');
     //Méthode normale
     /*if(isset($_POST['mail']) && isset($_POST['mdp']) && isset($_POST['num'])){
@@ -26,12 +25,17 @@
         if($bool){
             $res = $sel->fetchAll(PDO::FETCH_ASSOC);
             var_dump($res);
-        }
 
-        if(count($res) == 0){
-            $insert = "INSERT INTO utilisateur(mail_user, pwd_user, phone_user) VALUES (". $mail . ", " . $mdp . ", " .$num . ")";
-            return true;
-        } else return false;
+            if(count($res) == 0){
+                $insert = "INSERT INTO utilisateur(mail_user, pwd_user, phone_user) VALUES (:mail_u, :pwd_u, :phone_u)";
+                $ins = $pdo->prepare($insert);
+                $ins->bindParam(":mail_us", $mail);
+                $ins->bindParam(":pwd_u", $mdp);
+                $ins->bindParam(":phone_u", $num);
+                $ins->execute([$mail, $mdp, $num]);
+                require("../pages/page_login.php");
+            }
+        }
     }
 
 ?>
--- src/php_bdd/verif_login.php
+++ src/php_bdd/verif_login.php
@@ -1,4 +1,25 @@
 
 <?php
-    require('./config.php');
+    session_start();
+    
+
+    function verif_login($mail, $mdp){
+        require('./config.php');
+        
+        $select = "SELECT * FROM utilisateur WHERE mail_user=:mail_u AND pwd_user=:pwd_u";
+        $sel= $pdo->prepare($select);
+        $sel->bindParam(":mail_u",$mail);
+        $sel->bindParam(":pwd_u",$mdp);
+
+        $bool = $sel->execute();
+
+        if($bool){
+            $result = $sel->rowCount();
+            var_dump($result);
+
+            if($result !== 0){
+                return $sel->fetchAll(PDO::FETCH_ASSOC);
+            }
+        }
+    }
 ?>
